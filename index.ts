93fba941b6f1e936ff546b8ad24e9eb03b1ea3be
Josh McLain <joshmclain45@gmail.com>
2022-02-01 19:58:44 -0700

    connected to local db

--- index.ts
+++ index.ts
@@ -1,11 +1,19 @@
-import express, { Express } from 'express';
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import express, { Express } from "express";
+import mongoose from "mongoose";
 
-if (process.env.NODE_ENV !== 'production') {
-    dotenv.config();
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
 }
 
 const app: Express = express();
 const PORT: number = +process.env.PORT || 8080;
+// const databaseUri: string = process.env.DB_URI;
+const databaseUri = "mongodb://localhost/pcw";
+
+
+
+mongoose.connect(databaseUri, {}, () => console.log("database connected"));
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
