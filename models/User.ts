351c15d85348dcfe7f3e9a7b78827d27cb78ba70
Josh McLain <joshmclain45@gmail.com>
2022-02-02 17:54:44 -0700

    created user model, and updated linter

---
+++ models/User.ts
@@ -0,0 +1,14 @@
+import { Schema, Model, model } from "mongoose";
+import User from "../types/UserType";
+
+const UserSchema = new Schema<User>({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false },
+  isActive: { type: Boolean, default: true },
+//   playRecord: { type: Array },
+  registerDate: { type: Date, default: Date.now },
+});
+
+export const UserModel: Model<User> = model("User", UserSchema);
